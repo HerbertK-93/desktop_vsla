17e46e25149456670b8a99e7804794fe2873f1d1
HerbertK-93 <hezboncaleb@gmail.com>
2025-06-29 22:38:17 +0300

    renaming application to STINO VSLA SOFTWARE

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,4 +1,7 @@
 import 'package:drift/drift.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
